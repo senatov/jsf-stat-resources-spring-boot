13c46f65a2f8b879f16eae843b363c0bfdffe0f9
Iakov Senatov <javaEntwickler@gmail.com>
2020-06-11 02:32:04 +0200

    ok/tested/closed:polish

---
+++ src/main/java/de/senatov/drillstatic/ServletInitializer.java
@@ -0,0 +1,18 @@
+package de.senatov.drillstatic;
+
+
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+		return application.sources(JsfSpringBootApplication.class);
+	}
+
+}
